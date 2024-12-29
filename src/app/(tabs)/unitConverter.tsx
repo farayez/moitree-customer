308aaaf4861081682d526c2f167a3baa016414b8
Araf Farayez <araf.farayez@gmail.com>
2024-12-30 01:19:06 +0600

    improve unitConverter UI

--- src/app/(tabs)/unitConverter.tsx
+++ src/app/(tabs)/unitConverter.tsx
@@ -56,59 +56,65 @@ export default function UnitConverter() {
       </ThemedView>
 
       <ThemedView style={styles.converterContainer}>
-        <ThemedText type="subtitle">Input 1</ThemedText>
-        <View style={styles.dropdownContainer}>
-          <Picker
-            style={[
-              styles.dropdown,
-              Platform.select({
-                web: { padding: 12 },
-              }),
-            ]}
-            selectedValue={input1Unit}
-            onValueChange={(itemValue) => setInput1Unit(itemValue)}>
-            {unitOptions.map((option) => (
-              <Picker.Item key={option.name} label={option.label} value={option.name} />
-            ))}
-          </Picker>
+        <View>
+          <View style={styles.dropdownContainer}>
+            <Picker
+              style={[
+                styles.dropdown,
+                Platform.select({
+                  web: { padding: 12 },
+                }),
+              ]}
+              selectedValue={input1Unit}
+              onValueChange={(itemValue) => setInput1Unit(itemValue)}>
+              {unitOptions.map((option) => (
+                <Picker.Item key={option.name} label={option.label} value={option.name} />
+              ))}
+            </Picker>
+          </View>
+          <TextInput
+            style={styles.numericInput}
+            keyboardType="numeric"
+            onChangeText={(text) => {
+              setInput1Value(text);
+              setInput2Value(convertUnits(text, input1Unit, input2Unit));
+            }}
+            value={input1Value}
+          />
         </View>
-        <ThemedText type="subtitle">Value</ThemedText>
-        <TextInput
-          style={styles.numericInput}
-          keyboardType="numeric"
-          onChangeText={(text) => {
-            setInput1Value(text);
-            setInput2Value(convertUnits(text, input1Unit, input2Unit));
-          }}
-          value={input1Value}
-        />
+        <View style={styles.equalSignContainer}>
+          <ThemedText type="defaultSemiBold" style={styles.equalSignText}>
+            {' '}
+            ={' '}
+          </ThemedText>
+        </View>
 
-        <ThemedText type="subtitle">Input 2</ThemedText>
-        <View style={styles.dropdownContainer}>
-          <Picker
-            style={[
-              styles.dropdown,
-              Platform.select({
-                web: { padding: 12 },
-              }),
-            ]}
-            selectedValue={input2Unit}
-            onValueChange={(itemValue) => setInput2Unit(itemValue)}>
-            {unitOptions.map((option) => (
-              <Picker.Item key={option.name} label={option.label} value={option.name} />
-            ))}
-          </Picker>
+        <View style={styles.inputGroupContainer}>
+          <View style={styles.dropdownContainer}>
+            <Picker
+              style={[
+                styles.dropdown,
+                Platform.select({
+                  web: { padding: 12 },
+                }),
+              ]}
+              selectedValue={input2Unit}
+              onValueChange={(itemValue) => setInput2Unit(itemValue)}>
+              {unitOptions.map((option) => (
+                <Picker.Item key={option.name} label={option.label} value={option.name} />
+              ))}
+            </Picker>
+          </View>
+          <TextInput
+            style={styles.numericInput}
+            keyboardType="numeric"
+            onChangeText={(text) => {
+              setInput2Value(text);
+              setInput1Value(convertUnits(text, input2Unit, input1Unit));
+            }}
+            value={input2Value}
+          />
         </View>
-        <ThemedText type="subtitle">Value</ThemedText>
-        <TextInput
-          style={styles.numericInput}
-          keyboardType="numeric"
-          onChangeText={(text) => {
-            setInput2Value(text);
-            setInput1Value(convertUnits(text, input2Unit, input1Unit));
-          }}
-          value={input2Value}
-        />
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -137,12 +143,21 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginBottom: 16,
   },
+  equalSignContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 48, // Ensure it matches the height of the TextInput and Picker
+  },
+  equalSignText: {
+    fontSize: 24,
+    alignSelf: 'center', // Center align horizontally
+  },
+  inputGroupContainer: {},
   dropdownContainer: {
     borderRadius: 12,
     backgroundColor: 'rgba(255, 255, 255, 0.1)',
     borderColor: 'rgba(255, 255, 255, 0.2)',
     borderWidth: 1,
-    marginVertical: 8,
   },
   dropdown: {
     width: '100%',
@@ -160,7 +175,7 @@ const styles = StyleSheet.create({
   numericInput: {
     width: '100%',
     padding: 12,
-    marginVertical: 8,
+    marginTop: 8,
     borderRadius: 12,
     backgroundColor: 'rgba(255, 255, 255, 0.1)',
     borderColor: 'rgba(255, 255, 255, 0.2)',
@@ -202,5 +217,6 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     textTransform: 'uppercase',
     letterSpacing: 1,
+    textAlign: 'center', // Ensure text is centered
   },
 });
